5085fe7184358c14908ef4f5a5f35975b45ec82d
TK-ed <tharunstark11@gmail.com>
2024-02-03 18:58:37 +0530

    Add auth and completed homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,42 @@
+import { ArrowRightIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
-      <h1></h1>
+      <div className="flex flex-col lg:flex-row bg-[#1E1919] dark:bg-slate-800 items-center mt-1">
+        <div className="flex flex-col bg-[#2b2929] p-10 space-y-5 text-white dark:bg-slate-800">
+          <h1 className="text-5xl font-bold">
+            Welcome to <span className="sync">Sync.Sphere</span>
+            <br /> <br />
+            Where Your Data Finds Harmony in the Cloud.
+          </h1>
+          <p className="pb-20">
+            SyncSphere is your digital sanctuary, a cutting-edge storage web app
+            designed to seamlessly synchronize and safeguard your files in the
+            cloud. Effortlessly navigate the realms of data organization as
+            Sync.Sphere harmonizes simplicity with powerful features. Embrace
+            the future of cloud storage.
+          </p>
+          <h3 className="text-4xl mt-4">Embrace Sync.Sphere!!</h3>
+          <Link
+            href={"/dashboard"}
+            className="flex items-center p-5 cursor-pointer bg-blue-600 w-fit"
+          >
+            Try it for free!
+            <ArrowRightIcon className="ml-5" />
+          </Link>
+        </div>
+        <div className="bg-[#1E1919] dark:bg-slate-800 h-full p-10">
+          <video autoPlay loop muted className="rounded-xl">
+            <source
+              src="https://aem.dropbox.com/cms/content/dam/dropbox/warp/en-us/overview/lp-header-graphite200-1920x1080.mp4"
+              type="video/mp4"
+            />
+            Your browser doesnt support video tag
+          </video>
+        </div>
+      </div>
     </main>
   );
 }
