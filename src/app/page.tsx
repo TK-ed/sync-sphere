75b29950aa88e1fd25811609f74387870c55626f
TK-ed <tharunstark11@gmail.com>
2024-04-22 00:26:40 +0530

    Fixed responsive design issues for optimal display on mobile devices

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,15 +16,15 @@ export default function Home() {
     >
       <div className="flex flex-col lg:flex-row bg-[#1E1919] dark:bg-slate-800 items-center mt-1">
         <div className="flex flex-col bg-[#2b2929] p-10 space-y-5 text-white dark:bg-slate-800">
-          <h1 className="font-extrabold text-6xl bg-gradient-to-r from-blue-500 via-blue-400 to-blue-100 inline-block text-transparent bg-clip-text">
+          <h1 className="font-extrabold text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl bg-gradient-to-r from-blue-500 via-blue-400 to-blue-100 inline-block text-transparent bg-clip-text">
             <Type />
           </h1>
-          <h1 className="text-5xl font-bold">
+          <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold">
             <br />
             Where Your Data Finds Harmony in the Cloud.
           </h1>
           <TextGenerateEffectDemo />
-          <h3 className="text-4xl mt-4">Embrace Sync.Sphere!!</h3>
+          <h3 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl mt-4">Embrace Sync.Sphere!!</h3>
           <Link
             href={"/dashboard"}
             className="flex items-center p-5 cursor-pointer bg-blue-600 w-fit"
